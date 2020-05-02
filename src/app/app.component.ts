0b690e1a9ef435ea1d049817100f13c4c2994e28
Marina Melaré <melare.nina@gmail.com>
2020-05-02 18:34:46 -0300

    Reactive: Reacting to Status or Value Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,13 @@ export class AppComponent implements OnInit{
         gender: new FormControl('female', Validators.required),
         hobbies: new FormArray([])
     });
+
+    // this.signupForm.valueChanges.subscribe((value) => {
+    //   console.log(value);
+    // });
+    this.signupForm.statusChanges.subscribe((status) => {
+      console.log(status);
+    });
   }
 
   onSubmit(){
